60226d564df4c9861b0ba512a2ee29b8bf048d3c
fruitcakej <33979363+fruitcakej@users.noreply.github.com>
2018-03-10 14:50:04 +0100

    Building other activities

--- app/src/main/java/com/example/android/funkypod/MainActivity.java
+++ app/src/main/java/com/example/android/funkypod/MainActivity.java
@@ -7,8 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -21,26 +21,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
-
-        // Find the View that shows the data we require.
-        TextView infoTextView = (TextView) findViewById(R.id.info);
-
-        // Set a click listener on view
-        if (infoTextView != null)
-            infoTextView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    Intent moreInfoIntent = new Intent(MainActivity.this, MoreInfo.class);
-                    startActivity(moreInfoIntent);
-                    moreInfoIntent.putExtra(Mixes, R.id.mixName);
-                    moreInfoIntent.putExtra(Mixes, R.id.artistName);
-                    moreInfoIntent.putExtra(Mixes, R.id.info);
-                    moreInfoIntent.putExtra(Mixes, R.id.mix_icon);
-
-
-                }
-            });
 
 
         // Create an arraylist of the available mixes / podcasts available
@@ -85,9 +65,42 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listview_mixes);
         listView.setAdapter(mixAdapter);
 
+        /**
+         * Set onItemClick listener for specific row clicks
+         */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent moreInfoIntent = new Intent(MainActivity.this, MoreInfo.class);
+                startActivity(moreInfoIntent);
+
+            }
+        });
+
+        /**
+         * Do I need this method ?
+         */
+
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
-    @Override
+
+
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
