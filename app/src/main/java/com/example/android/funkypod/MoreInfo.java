1e0307807eef9c487f6acbafbc21ccbd3bb16aab
fruitcakej <33979363+fruitcakej@users.noreply.github.com>
2018-03-10 15:34:40 +0100

    Building other activities

--- app/src/main/java/com/example/android/funkypod/MoreInfo.java
+++ app/src/main/java/com/example/android/funkypod/MoreInfo.java
@@ -1,13 +1,11 @@
 package com.example.android.funkypod;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MoreInfo extends AppCompatActivity {
 
@@ -39,6 +37,7 @@ public class MoreInfo extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.showPlaylistIcon) {
+            startActivity(new Intent(this, ViewPlaylist.class));
             return true;
         }
 
